2cf53be1d8fece737f289ef88b9d1b95e7450a30
Charlie Burrows <charlie.burrows@gmail.com>
2014-07-08 07:54:18 -0400

    Added header files, oops

---
+++ mass.h
@@ -0,0 +1,55 @@
+#ifndef MASS
+#define MASS
+
+#include <SDL.h> 
+#include <SDL2_gfxPrimitives.h>
+
+#include "vec3.h"
+
+/* MASS *************************/
+
+typedef struct mass {
+  float m; /* as in f=ma */
+  v3 pos;
+  v3 prev_pos;
+  v3 acc;
+  char fixed;
+} mass;
+
+void print_mass(mass *m);
+void draw_mass(SDL_Renderer *ren, mass *m);
+void step_mass(mass *m, float dt);
+void gravity(mass *m);
+
+/* CONSTRAINT ******************/
+
+typedef enum { DIST_EQ, DIST_GT, DIST_LT, XGT, YGT, ZGT, XLT, YLT, ZLT, XEQ, YEQ, ZEQ } constraint_type;
+typedef struct constraint {
+  constraint_type type;
+  mass **masses;
+  int nmasses;
+  float val;
+} constraint;
+
+void print_constraint(constraint *c);
+void run_constraint(constraint *c);
+
+/* MODEL ************/
+
+typedef struct model {
+  mass *masses;
+  int nmasses;
+
+  constraint *constraints;
+  int nconstraints;
+} model;
+
+void print_model(model *m);
+void draw_model(SDL_Renderer *ren, model *m);
+void translate_model(model *m, v3 *t);
+void step_model(model *m, float dt);
+void link_masses_dist_eq(float dist, mass *m1, mass *m2, constraint *c);
+void make_grid(int x, int y, float spacing, model *model);
+void masstest();
+
+#endif
---
+++ vec3.h
@@ -0,0 +1,20 @@
+#ifndef VEC3
+#define VEC3
+
+typedef struct v3 { 
+  float x;
+  float y;
+  float z;
+} v3;
+
+void v3add(const v3 *v1, const v3 *v2, v3 *out);
+void v3sub(const v3 *v1, const v3 *v2, v3 *out);
+void v3mul(const v3 *v, const float m, v3 *out);
+void v3div(const v3 *v, const float m, v3 *out);
+float v3mag(const v3 *v);
+void v3norm(const v3 *v, v3 *out);
+float v3dist(const v3 *v1, const v3 *v2);
+
+void v3test();
+
+#endif

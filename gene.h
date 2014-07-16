eca6575726c273f9fda4efcfb4c1362461744e53
Charlie Burrows <charlie.burrows@gmail.com>
2014-07-15 20:23:57 -0400

    forgot to add the header file ... AGAIN

---
+++ gene.h
@@ -0,0 +1,20 @@
+#include "mass.h"
+
+#define NUM_MASSES 50
+#define NUM_CONSTRAINTS 250
+typedef struct gene {
+  float masses[NUM_MASSES*4];
+  int constraints[NUM_CONSTRAINTS*2];
+} gene;
+
+void mass_to_gene(mass *m, float *gene);
+void gene_to_mass(float *gene, mass *m);
+void constraint_to_gene(constraint *c, mass *m, int *gene);
+void gene_to_constraint(int *gene, constraint *c, mass *m);
+void model_to_gene(model *m, gene *g);
+void gene_to_model(gene *g, model *m);
+void crossover(gene *g1, gene *g2, gene *res);
+void init_gene(gene *g);
+void init_pool(gene **genes, int ngenes);
+void start_GA();
+void genetest();
